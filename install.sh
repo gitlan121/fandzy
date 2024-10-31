ab43cee2975445d67564a0473e6be0bb15246e80
gitlan121 <ulumolam@gmail.com>
2024-10-31 17:09:57 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ check_token() {
   echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
   read -r USER_TOKEN
 
-  if [ "$USER_TOKEN" = "FandGG" ]; then
+  if [ "$USER_TOKEN" = "FandGgggG" ]; then
     echo -e "${GREEN}AKSES BERHASIL${NC}}"
   else
     echo -e "${GREEN}Buy dulu Gih Ke Lanhost${NC}"
